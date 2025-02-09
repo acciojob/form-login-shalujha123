6c6eb570443ec1725d123e3c6ea502c4e8c05e88
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-09 22:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
+let form = document.getElementById("form1")
+function getFormvalue(e) {
     //Write your code here
-
+	e.preventDefault()
+	alert(form.fname.value+" "+form.lname.value)
 }
